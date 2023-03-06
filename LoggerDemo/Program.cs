12071993411e75c3d146bb3153ba1a3721570351
BashaVali <bashavali222@gmail.com>
2023-03-07 02:54:27 +0530

    Logger Demo

---
+++ LoggerDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LoggerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers obj = new AddNumbers();
+            obj.Sum(2, 0);
+            Console.WriteLine("This is a Logger Demo and Logs generated Successfully");
+            Console.ReadKey();
+        }
+    }
+}
